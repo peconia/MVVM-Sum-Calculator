b0b8ffce547c68c217661c6667c45e9361a1a61a
Paivi Suomela <paivi.e.suomela@gmail.com>
2015-11-29 17:24:44 +0000

    initial commit, calculator is working

---
+++ README.md
@@ -0,0 +1,3 @@
+# MVVM Sum Calculator
+
+This is a Windows WPF application made to practise MVVM using Prism 6.
---
+++ SumMvvm/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SumMvvm.ViewModels
+{
+    public class MainWindowViewModel: BindableBase
+    {
+        private int _x;
+        private int _y;
+        private int _z;
+
+        public int x
+        {
+            get { return _x; }
+            set
+            {
+                SetProperty(ref _x, value);
+            }
+        }
+
+        public int y
+        {
+            get { return _y; }
+            set
+            {
+                SetProperty(ref _y, value);
+            }
+        }
+        
+        public int z 
+        {
+            get { return _z; }
+            set
+            {
+                SetProperty(ref _z, value);
+            }
+        }
+
+        public ICommand SumCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            SumCommand = new DelegateCommand(onSum);
+        }
+
+        private void onSum()
+        {
+            z = x + y;
+        }
+
+    }
+}
